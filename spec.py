541315fe60d70a6c3da785ac02a60c6d7e950b7c
bgreene <bgreene777@gmail.com>
2023-02-15 21:34:31 +0000

    add 2d autocorr function that operates very similarly to 1d version; TODO: update x,y coordinates to more accurately represent positive and negative dx, dy

--- spec.py
+++ spec.py
@@ -10,7 +10,7 @@
 # --------------------------------
 import xarray as xr
 import numpy as np
-from numpy.fft import fft, ifft
+from scipy.fft import fft, ifft, fft2, ifft2
 from dask.diagnostics import ProgressBar
 from scipy.signal import detrend
 # --------------------------------
@@ -38,8 +38,6 @@ def autocorr_1d(dnc, df, detrend_first=False):
     for jt in range(df.time.size):
         # loop over variables
         for v in vall:
-            # empty PSD array
-            PSD = np.zeros((df.nx, df.ny, df.nz), dtype=np.float64)
             # grab data for processing
             din = df[v].isel(time=jt).to_numpy()
             # detrend
@@ -73,6 +71,60 @@ def autocorr_1d(dnc, df, detrend_first=False):
     
     # save nc file
     fsave = f"{dnc}R_1d.nc"
+    print(f"Saving file: {fsave}")
+    with ProgressBar():
+        Rsave.to_netcdf(fsave, mode="w")
+    
+    print("Finished computing autocorrelation functions!")
+    return
+# --------------------------------
+def autocorr_2d(dnc, df):
+    """Input 4D xarray Dataset with loaded LES data to calculate
+    2d autocorrelation function in x-y planes, then average in
+    time. Calculate for u, v, w, theta, u_rot, v_rot.
+    Save netcdf file in dnc.
+
+    :param str dnc: absolute path to netcdf directory for saving new file
+    :param Dataset df: 4d (time,x,y,z) xarray Dataset for calculating
+    """
+    # variables to loop over for calculations
+    vall = ["u", "v", "w", "theta", "u_rot", "v_rot"]
+    # define dictionary of empty arrays to store data
+    acf_all = {}
+    for v in vall:
+        acf_all[v] = np.zeros((df.nx, df.ny, df.nz), dtype=np.float64)
+
+    # BIG LOOP over time
+    for jt in range(df.time.size):
+        # loop over variables
+        for v in vall:
+            # grab data for processing
+            din = df[v].isel(time=jt).to_numpy()
+            # normalize
+            d = (din - din.mean(axis=(0,1))) / d.std(axis=(0,1))
+            # forward FFT
+            f = fft2(d, axes=(0,1))
+            # calculate PSD
+            PSD = np.abs(f) ** 2.
+            # ifft to get acf
+            R = np.real( ifft(PSD, axes=(0,1)) ) / df.nx / df.ny
+            # calculate mean along y-axis and assign to acf_all
+            acf_all[v] += R
+    
+    # loop over vall and normalize by nt to get time average
+    for v in vall:
+        acf_all[v] /= df.time.size
+
+    # construct Dataset to save
+    Rsave = xr.Dataset(data_vars=None, coords=dict(x=df.x, y=df.y, z=df.z), 
+                       attrs=df.attrs)
+    # loop over vars in vall and store
+    for v in vall:
+        Rsave[v] = xr.DataArray(data=acf_all[v], dims=("x","y","z"), 
+                                coords=dict(x=Rsave.x, y=Rsave.y, z=Rsave.z))
+    
+    # save nc file
+    fsave = f"{dnc}R_2d.nc"
     print(f"Saving file: {fsave}")
     with ProgressBar():
         Rsave.to_netcdf(fsave, mode="w")
