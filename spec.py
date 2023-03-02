e1a7f679d3988ffa284581b4d22e2e2c7868af78
bgreene <bgreene777@gmail.com>
2023-03-02 20:13:55 +0000

    rewrite autocorr_1d using xrft to preserve positive and negative frequencies natively when saving netcdf file

--- spec.py
+++ spec.py
@@ -8,15 +8,16 @@
 # spectral analysis of LES output including:
 # autocorrelation, spectral interpolation, power spectral density
 # --------------------------------
+import xrft
 import xarray as xr
 import numpy as np
-from scipy.fft import fft, ifft, fft2, ifft2
+from scipy.fft import fft, ifft, fft2, ifft2, fftshift
 from dask.diagnostics import ProgressBar
 from scipy.signal import detrend
 # --------------------------------
 # Begin Defining Functions
 # --------------------------------
-def autocorr_1d(dnc, df, detrend_first=False):
+def autocorr_1d(dnc, df, detrend="constant"):
     """Input 4D xarray Dataset with loaded LES data to calculate
     autocorrelation function along x-direction, then average in
     y and time. Calculate for u, v, w, theta, u_rot, v_rot.
@@ -24,52 +25,34 @@ def autocorr_1d(dnc, df, detrend_first=False):
 
     :param str dnc: absolute path to netcdf directory for saving new file
     :param Dataset df: 4d (time,x,y,z) xarray Dataset for calculating
-    :param bool detrend_first: linear detrend along x-axis before processing,\
-        default=False
+    :param str detrend: how to detrend along x-axis before processing,\
+        default="constant" (also accepts "linear")
     """
+    # construct Dataset to save
+    Rsave = xr.Dataset(data_vars=None, attrs=df.attrs)
+    # add additional attr for detrend_first
+    Rsave.attrs["detrend_type"] = str(detrend)
+
     # variables to loop over for calculations
     vall = ["u", "v", "w", "theta", "u_rot", "v_rot"]
-    # define dictionary of empty arrays to store data
-    acf_all = {}
-    for v in vall:
-        acf_all[v] = np.zeros((df.nx, df.nz), dtype=np.float64)
 
-    # BIG LOOP over time
-    for jt in range(df.time.size):
-        # loop over variables
-        for v in vall:
-            # grab data for processing
-            din = df[v].isel(time=jt).to_numpy()
-            # detrend
-            if detrend_first:
-                d = detrend(din, axis=0, type="linear")
-                d /= d.std(axis=0)
-            else:
-                d = (din - din.mean(axis=0)) / din.std(axis=0)
-            # forward FFT
-            f = fft(d, axis=0)
-            # calculate PSD
-            PSD = np.abs(f) ** 2.
-            # ifft to get acf
-            R = np.real( ifft(PSD, axis=0) ) / df.nx
-            # calculate mean along y-axis and assign to acf_all
-            acf_all[v] += np.mean(R, axis=1)
-    
-    # loop over vall and normalize by nt to get time average
+    # loop over variables
     for v in vall:
-        acf_all[v] /= df.time.size
-
-    # construct Dataset to save
-    Rsave = xr.Dataset(data_vars=None, 
-                       coords=dict(x=df.x[:df.nx//2], z=df.z), 
-                       attrs=df.attrs)
-    # add additional attr for detrend_first
-    Rsave.attrs["detrend_first"] = str(detrend_first)
-    # loop over vars in vall and store
-    for v in vall:
-        Rsave[v] = xr.DataArray(data=acf_all[v][:df.nx//2,:], 
-                                dims=("x", "z"), 
-                                coords=dict(x=Rsave.x, z=Rsave.z))
+        # grab data
+        din = df[v]
+        # detrend
+        # subtract mean by default, or linear if desired
+        dfluc = xrft.detrend(din, dim="x", detrend_type=detrend)
+        # normalize by standard deviation
+        dnorm = dfluc / dfluc.std(dim="x")
+        # calculate PSD using xrft
+        PSD = xrft.power_spectrum(dnorm, dim="x", true_phase=True, true_amplitude=True)
+        # take real part of ifft to return ACF
+        R = xrft.ifft(PSD, dim="freq_x", true_phase=True, true_amplitude=True, lag=0).real
+        # average in y and time
+        R_ytavg = R.mean(dim=("y","time"))
+        # store in Rsave
+        Rsave[v] = R_ytavg
     
     # save nc file
     fsave = f"{dnc}R_1d.nc"
