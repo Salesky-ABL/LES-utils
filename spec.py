375b345314e91e87d87101f65b39e83264b9cd79
bgreene <bgreene777@gmail.com>
2023-05-03 22:34:47 +0000

    add amp_mod function but does not seem to work properly yet??

--- spec.py
+++ spec.py
@@ -12,6 +12,7 @@ import os
 import xrft
 import xarray as xr
 import numpy as np
+from scipy.signal import hilbert
 from dask.diagnostics import ProgressBar
 # --------------------------------
 # Begin Defining Functions
@@ -182,4 +183,118 @@ def spectrogram(dnc, df, detrend="constant"):
         Esave.to_netcdf(fsave, mode="w")
     
     print("Finished computing spectrograms!")
+    return
+# --------------------------------
+def amp_mod(dnc, ts, s):
+    """Input timeseries xarray Dataset and stats file 
+    with loaded LES data to calculate amplitude modulation 
+    coefficients for all of: u, v, w, theta, q, uw, tw, qw, tq.
+    Automatically use cutoff wavelength of lambda_c = z_i,
+    convert to temporal frequency with Taylor's hypothesis.
+    Save netcdf file in dnc.
+
+    :param str dnc: absolute path to netcdf directory for saving new file
+    :param Dataset ts: timeseries xarray Dataset for calculating
+    :param Dataset s: mean statistics file for reference
+    """
+    print("Begin calculating amplitude modulation coefficients")
+    # lengthscale of filter to separate large and small scales
+    delta = s.h.values
+    # cutoff frequency from Taylor
+    f_c = 1./(delta/ts.u_mean_rot)
+    # names of variables to loop over: first half are detrended
+    var_ts = ["udr", "vdr", "wd", "td", "qd", "uw", "tw", "qw", "tq"]
+    # initialize empty dictionary to hold FFT timseries
+    f_all = {}
+    print("Lowpass filter signals to separate large and small scale components")
+    # loop over var_ts and take forward FFT
+    for v in var_ts:
+        f_all[v] = xrft.fft(ts[v], dim="t", true_phase=True, true_amplitude=True)
+        # now loop over heights and lowpass filter
+        for jz in range(ts.nz):
+            # can do this all in one line
+            # set high frequencies equal to zero -- sharp spectral filter
+            # only keep -f_c < freq_t < f_c
+            f_all[v][:,jz] = f_all[v][:,jz].where((f_all[v].freq_t < f_c.isel(z=jz)) &\
+                                                  (f_all[v].freq_t >-f_c.isel(z=jz)),
+                                                  other=0.)
+            
+    # inverse FFT to get large-scale component of signal
+    # initialize empty dictionaries for large and small scale components
+    ts_l = {}
+    ts_s = {}
+    for v in var_ts:
+        ts_l[v] = xrft.ifft(f_all[v], dim="freq_t", true_phase=True, true_amplitude=True,
+                            lag=f_all[v].freq_t.direct_lag).real
+        # reset time coordinate
+        ts_l[v]["t"] = ts.t
+        # calculate small-scale component by subtracting large-scale from full
+        ts_s[v] = ts[v] - ts_l[v]
+
+    print("Calculate envelope of small-scale signal from Hilbert transform")
+    # calculate envelope of small-scale signal from Hilbert transform
+    # dictionary of envelopes
+    Env = {}
+    for v in var_ts:
+        Env[v] = xr.DataArray(data=np.abs(hilbert(ts_s[v], axis=0)),
+                              coords=dict(t=ts_s[v].t, z=ts_s[v].z))
+        
+    # lowpass filter the small-scale envelopes
+    # dictionary of FFT envelopes
+    f_E = {}
+    # FFT the envelopes
+    for v in var_ts:
+        f_E[v] = xrft.fft(Env[v], dim="t", true_phase=True, 
+                          true_amplitude=True, detrend="linear")
+        # loop over heights and lowpass filter (same as above)
+        for jz in range(ts.nz):
+            f_E[v][:,jz] = f_E[v][:,jz].where((f_E[v].freq_t < f_c.isel(z=jz)) &\
+                                              (f_E[v].freq_t <-f_c.isel(z=jz)),
+                                              other=0.)
+    
+    # inverse FFT the filtered envelopes
+    E_filt = {}
+    for v in var_ts:
+        E_filt[v] = xrft.ifft(f_E[v], dim="freq_t", true_phase=True,
+                              true_amplitude=True, 
+                              lag=f_E[v].freq_t.direct_lag).real
+        # reset time coordinate
+        E_filt[v]["t"] = ts.t
+
+    # AM Coefficients -----------
+    print("Computing AM coefficients")
+    # new dataset to hold correlation coefficients
+    R = xr.Dataset(data_vars=None,
+                   coords=dict(z=ts.z),
+                   attrs=ts.attrs)
+    # add delta as attr
+    R.attrs["cutoff"] = delta
+    # list of variable names for new save notation (can zip with var_ts to match)
+    vsave = ["u", "v", "w", "t", "q", "uw", "tw", "qw", "tq"]
+    # correlation between large scale u and filtered envelope of small-scale variable
+    for vts, vv in zip(var_ts, vsave):
+        key = f"ul_E{vv}"
+        R[key] = xr.corr(ts_l["udr"], E_filt[vts], dim="t")
+    # correlation between large scale w and filtered envelope of small-scale variable
+    for vts, vv in zip(var_ts, vsave):
+        key = f"wl_E{vv}"
+        R[key] = xr.corr(ts_l["wd"], E_filt[vts], dim="t")
+    # correlation between large scale theta and filtered envelope of small-scale variable
+    for vts, vv in zip(var_ts, vsave):
+        key = f"tl_E{vv}"
+        R[key] = xr.corr(ts_l["td"], E_filt[vts], dim="t")
+    # correlation between large scale q and filtered envelope of small-scale variable
+    for vts, vv in zip(var_ts, vsave):
+        key = f"ql_E{vv}"
+        R[key] = xr.corr(ts_l["qd"], E_filt[vts], dim="t")
+
+    # save file
+    fsavenc = f"{dnc}AM_coefficients.nc"
+    # delete old file for saving new one
+    if os.path.exists(fsavenc):
+        os.system(f"rm {fsavenc}")
+    print(f"Saving file: {fsavenc}")
+    with ProgressBar():
+        R.to_netcdf(fsavenc, mode="w")
+
     return
