6f440cf3a4ab7f9d078ac0c881a2a2e9cf54f78a
bgreene <bgreene777@gmail.com>
2023-06-02 20:04:05 +0000

    new spectrogram function for rotated data; realize after finishing that the grid is NOT irregular in rotated frame...so basically need to redo mostof this.

--- spec.py
+++ spec.py
@@ -12,6 +12,7 @@ import os
 import xrft
 import xarray as xr
 import numpy as np
+from scipy.fft import fftfreq, fftshift
 from scipy.signal import hilbert
 from dask.diagnostics import ProgressBar
 # --------------------------------
@@ -183,6 +184,126 @@ def spectrogram(dnc, df, detrend="constant"):
         Esave.to_netcdf(fsave, mode="w")
     
     print("Finished computing spectrograms!")
+    return
+# --------------------------------
+def spectrogram_rot(dnc, t0, t1, dt, nx, nz, detrend="constant"):
+    """Purpose: calculate spectrograms using rotated and irregular
+    grids in x,y,z. Average in y_hat, interpolate to consistent k_x 
+    grid then average in time.
+    Calculate for u', w', theta', q', u'w', theta'w', q'w', theta'q'.
+    Save netcdf file in dnc.
+    :param str dnc: absolute path to netcdf directory
+    :param int t0: starting timestep
+    :param int t1: ending timestep
+    :param int dt: number of files between timesteps
+    :param int nx: number of grid points in x-dimension
+    :param int nz: number of grid points in z-dimension
+    """
+    # determine list of file timesteps from input
+    timesteps = np.arange(t0, t1+1, dt, dtype=np.int32)
+    # determine files to read from timesteps
+    fall = [f"{dnc}all_{tt:07d}_rot.nc" for tt in timesteps]
+    # number of timesteps for making new arrays
+    nt = len(timesteps)
+    # use nx to determine index for positive frequencies
+    jfreq = nx//2 + 1
+    # also can use to determine number of frequencies to store
+    nfreq = nx - jfreq
+    # initialize dictionary with empty arrays to store for time averaging
+    specall = {}
+    # variables to loop over for calculations: spectra
+    vall = ["u_rot", "w", "theta", "q"]
+    vsave = ["uu", "ww", "tt", "qq"]
+    # variables to loop over for cross spectra
+    vall2 = [("u_rot", "w"), ("theta", "w"), ("q", "w"), ("theta", "q")]
+    vsave2 = ["uw", "tw", "qw", "tq"]
+    # loop over vsave + vsave2 and initialize empty arrays
+    for var in vsave+vsave2:
+        specall[var] = np.zeros((nfreq, nz, nt), dtype=np.float64)
+    # BEGIN TIME LOOP
+    for jt, ff in enumerate(fall):
+        # load file
+        print(f"Loading file: {ff}")
+        d = xr.load_dataset(ff)
+        # look at how dx change with height
+        dx = np.zeros(nz)
+        # use this info to calculate wavenumbers of *rotated grid*
+        khat = np.zeros((nx, nz), dtype=np.float64)
+        for jz in range(nz):
+            dx[jz] = np.diff(d.rx.isel(z=jz))[0]
+            khat[:,jz] = fftshift(fftfreq(nx, dx[jz]))
+        # reference wavenumbers from base grid
+        kref = fftshift(fftfreq(d.nx, d.dx))
+        # LOOP OVER VARIABLES FOR SPECTRA
+        for v, vs in zip(vall, vsave):
+            # grab data
+            din = d[v].drop(["xhat","yhat"])
+            # subtract mean by default, or linear if desired
+            dfluc = xrft.detrend(din, dim="jx", detrend_type=detrend)
+            # normalize by standard deviation
+            dnorm = dfluc / dfluc.std(dim="jx")
+            # calculate PSD using xrft
+            PSD = xrft.power_spectrum(dnorm, dim="jx", true_phase=True, 
+                                      true_amplitude=True)
+            # average in y, convert to numpy, only take real
+            PSD_yavg = PSD.mean(dim="jy").to_numpy().real
+            # interpolate to wavenumber of original grid for consistency
+            PSD_int = np.zeros((nfreq, nz), dtype=np.float64)
+            # loop over heights and interp
+            for jz in range(nz):
+                PSD_int[:,jz] = np.interp(kref[jfreq:], khat[jfreq:,jz],
+                                          PSD_yavg[jfreq:,jz], 
+                                          left=np.nan, right=np.nan)
+            # store PSD_int in corresponding array in specall
+            specall[vs][:,:,jt] = PSD_int
+        # LOOP OVER VARIABLES FOR COSPECTRA
+        for v, vs in zip(vall2, vsave2):
+                # grab data
+                din1 = d[v[0]].drop(["xhat","yhat"])
+                din2 = d[v[1]].drop(["xhat","yhat"])
+                # detrend
+                # subtract mean by default, or linear if desired
+                dfluc1 = xrft.detrend(din1, dim="jx", detrend_type=detrend)
+                dfluc2 = xrft.detrend(din2, dim="jx", detrend_type=detrend)
+                # normalize by standard deviation
+                dnorm1 = dfluc1 / dfluc1.std(dim="jx")
+                dnorm2 = dfluc2 / dfluc2.std(dim="jx")
+                # calculate cross spectrum using xrft
+                PSD = xrft.cross_spectrum(dnorm1, dnorm2, dim="jx", 
+                                          scaling="density",
+                                          true_phase=True, true_amplitude=True)
+                 # average in y, convert to numpy, only take real
+                PSD_yavg = PSD.mean(dim="jy").to_numpy().real
+                # interpolate to wavenumber of original grid for consistency
+                PSD_int = np.zeros((nfreq, nz), dtype=np.float64)
+                # loop over heights and interp
+                for jz in range(nz):
+                    PSD_int[:,jz] = np.interp(kref[jfreq:], khat[jfreq:,jz],
+                                              PSD_yavg[jfreq:,jz], 
+                                              left=np.nan, right=np.nan)
+                # store PSD_int in corresponding array in specall
+                specall[vs][:,:,jt] = PSD_int
+    # OUTSIDE TIME LOOP
+    # need to average everything in specall across time dimension (axis=2)
+    # also convert to DataArray and store in Esave to output as netcdf file
+    Esave = xr.Dataset(data_vars=None, attrs=d.attrs)
+    # add additional attr for detrend_first
+    Esave.attrs["detrend_type"] = str(detrend)
+    # loop over all vsave+vsave2 and average
+    print("Average in time...")
+    for var in vsave+vsave2:
+        Esave[var] = xr.DataArray(data=np.nanmean(specall[var], axis=2),
+                                  coords=dict(freq_x=kref[jfreq:], z=d.z))
+    # save file and return
+    fsave = f"{dnc}spectrogram_rot.nc"
+    # delete old file for saving new one
+    if os.path.exists(fsave):
+        os.system(f"rm {fsave}")
+    print(f"Saving file: {fsave}")
+    with ProgressBar():
+        Esave.to_netcdf(fsave, mode="w")
+    
+    print("Finished computing rotated spectrograms!")
     return
 # --------------------------------
 def amp_mod(dnc, ts, s):
